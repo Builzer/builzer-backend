bac18a5f7635c501e3c1f1449b06c4a223f43886
wakkpu <ho0524870@gmail.com>
2024-07-01 23:58:58 +0900

    [BLZR-23] 외부 api 호출부 response 객체

--- src/main/kotlin/com/builzer/backend/member/adapter/out/external/response/OAuthExternalResponse.kt
+++ src/main/kotlin/com/builzer/backend/member/adapter/out/external/response/OAuthExternalResponse.kt
@@ -1,4 +1,14 @@
 package com.builzer.backend.member.adapter.out.external.response
 
-class OAuthExternalResponse {
-}
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class OAuthExternalResponse(
+    @JsonProperty("access_token")
+    val accessToken: String,
+
+    @JsonProperty("scope")
+    val scope: String,
+
+    @JsonProperty("token_type")
+    val tokenType: String,
+)
