a4f82293bb08c40529bd91a700d5deb0376450e7
wakkpu <ho0524870@gmail.com>
2024-07-01 23:56:57 +0900

    [BLZR-23] Oauth service

--- src/main/kotlin/com/builzer/backend/member/application/service/OAuthService.kt
+++ src/main/kotlin/com/builzer/backend/member/application/service/OAuthService.kt
@@ -1,6 +1,8 @@
 package com.builzer.backend.member.application.service
 
+import com.builzer.backend.global.util.JwtUtil
 import com.builzer.backend.member.application.port.`in`.OAuthUseCase
+import com.builzer.backend.member.application.port.`in`.command.CreateOrReadCommand
 import com.builzer.backend.member.application.port.`in`.command.OAuthCommand
 import com.builzer.backend.member.application.port.`in`.response.OAuthResponse
 import com.builzer.backend.member.application.port.out.MemberPort
@@ -10,16 +12,45 @@ import org.springframework.stereotype.Service
 @Service
 class OAuthService(
     private val memberPort: MemberPort,
-    private val oAuthExternalRequestPort: OAuthExternalRequestPort
+    private val oAuthExternalRequestPort: OAuthExternalRequestPort,
+    private val jwtUtil: JwtUtil
 ) : OAuthUseCase {
     override fun signInOrSignUp(oAuthCommand: OAuthCommand): OAuthResponse {
+        // retrieve access token from GitHub
         val githubAccessToken = oAuthExternalRequestPort.requestGithubAccessToken(oAuthCommand.code)
 
-        val userInfo = oAuthExternalRequestPort.requestUserInfo(githubAccessToken)
+        // retrieve user info from GitHub with access token
+        val userInfoFromGithub = oAuthExternalRequestPort.requestUserInfo(githubAccessToken)
 
-        val member = memberPort.createOrRead()
+        val createOrReadCommand = CreateOrReadCommand(
+            githubAccessToken = githubAccessToken,
+            name = userInfoFromGithub.name,
+            email = userInfoFromGithub.email
+        )
 
-        TODO("create token and set userinfo into security context")
+        // get member info
+        val member = memberPort.createOrRead(createOrReadCommand)
 
+        // generate access token
+        val accessToken =
+            jwtUtil.generateAccessToken(
+                email = member.gitEmail,
+                memberId = member.id,
+                gitAccessToken = member.gitAccessToken
+            )
+
+        // generate refresh token
+        val refreshToken =
+            jwtUtil.generateRefreshToken(
+                email = member.gitEmail,
+                memberId = member.id,
+                gitAccessToken = member.gitAccessToken
+            )
+
+        return OAuthResponse(
+            member = member,
+            accessToken = accessToken,
+            refreshToken = refreshToken
+        )
     }
 }
