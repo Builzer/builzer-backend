960056bc1a45656e07df6a205da29f52683a5f89
wakkpu <ho0524870@gmail.com>
2024-07-01 23:56:41 +0900

    [BLZR-23] member 관련 영속성 계층부

--- src/main/kotlin/com/builzer/backend/member/adapter/out/persistence/MemberPersistenceAdapter.kt
+++ src/main/kotlin/com/builzer/backend/member/adapter/out/persistence/MemberPersistenceAdapter.kt
@@ -1,8 +1,12 @@
 package com.builzer.backend.member.adapter.out.persistence
 
+import com.builzer.backend.member.adapter.out.persistence.entity.MemberJpaEntity
 import com.builzer.backend.member.adapter.out.persistence.repository.MemberJpaRepository
+import com.builzer.backend.member.application.port.`in`.command.CreateOrReadCommand
 import com.builzer.backend.member.application.port.out.MemberPort
 import com.builzer.backend.member.domain.Member
+import com.builzer.backend.member.util.MemberMapper
+import org.mapstruct.factory.Mappers
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,7 +15,22 @@ import org.springframework.transaction.annotation.Transactional
 class MemberPersistenceAdapter(
     private val memberJpaRepository: MemberJpaRepository
 ) : MemberPort {
-    override fun createOrRead(): Member {
-        TODO("create or read")
+    companion object {
+        private val mapper = Mappers.getMapper(MemberMapper::class.java)
+    }
+
+    @Transactional
+    override fun createOrRead(command: CreateOrReadCommand): Member {
+        // save() if findByGitEmail() is null
+        val memberEntity =
+            memberJpaRepository.findByGitEmail(command.email) ?: memberJpaRepository.save(
+                MemberJpaEntity(
+                    name = command.name,
+                    gitEmail = command.email,
+                    gitAccessToken = command.githubAccessToken
+                )
+            )
+
+        return mapper.entityToDomain(memberEntity)
     }
 }
--- src/main/kotlin/com/builzer/backend/member/application/port/out/MemberPort.kt
+++ src/main/kotlin/com/builzer/backend/member/application/port/out/MemberPort.kt
@@ -1,7 +1,8 @@
 package com.builzer.backend.member.application.port.out
 
+import com.builzer.backend.member.application.port.`in`.command.CreateOrReadCommand
 import com.builzer.backend.member.domain.Member
 
 interface MemberPort {
-    fun createOrRead(): Member
+    fun createOrRead(command: CreateOrReadCommand): Member
 }
