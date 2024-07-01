2ab151aaeae902e697837940a93005bc5f3859a5
wakkpu <ho0524870@gmail.com>
2024-07-01 23:57:09 +0900

    [BLZR-23] 임시 security config

--- src/main/kotlin/com/builzer/backend/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/builzer/backend/global/config/security/SecurityConfig.kt
@@ -1,10 +1,13 @@
 package com.builzer.backend.global.config.security
 
+import com.builzer.backend.global.util.JwtUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +17,20 @@ class SecurityConfig {
         http
             .authorizeHttpRequests { request ->
                 request
-                    .requestMatchers("/h2-console/**").permitAll()
-                    .requestMatchers("/favicon.ico").permitAll()
+                    .requestMatchers("/**").permitAll()
                     .anyRequest().authenticated()
             }
             .csrf { csrf ->
-                csrf.ignoringRequestMatchers("/h2-console/**")
+                csrf.disable()
             }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+
+        http.addFilterAt(
+            JwtAuthenticationFilter(JwtUtil()),
+            UsernamePasswordAuthenticationFilter::class.java
+        )
 
         http.headers { header ->
             header.frameOptions { frameOptions ->
