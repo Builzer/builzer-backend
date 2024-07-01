9374fc4076b519e7deab26b20e3249f2173131ff
wakkpu <ho0524870@gmail.com>
2024-07-01 23:51:24 +0900

    [BLZR-23] 의존성 추가 - jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,14 @@ dependencies {
     // WireMock
     testImplementation("org.springframework.cloud:spring-cloud-contract-wiremock:4.0.4")
 
+    // feign
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("io.github.openfeign:feign-okhttp")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 kotlin {
