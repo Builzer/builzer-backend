7e98bcbd42cc263759c4bee835fd0d5dd298a0ea
MJun111 <kmj0568@gmail.com>
2024-07-07 15:54:09 +0900

    [BLZR-37] Github Client Config 추가

--- src/main/kotlin/com/builzer/backend/project/adapter/out/client/GithubRepoInfoClient.kt
+++ src/main/kotlin/com/builzer/backend/project/adapter/out/client/GithubRepoInfoClient.kt
@@ -1,45 +1,40 @@
 package com.builzer.backend.project.adapter.out.client
 
+import com.builzer.backend.global.config.feign.GithubClientConfig
 import com.builzer.backend.project.adapter.out.client.response.*
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(
     name = "GithubRepoInfoClient",
-    url = "https://api.github.com"
+    url = "https://api.github.com",
+    configuration = [GithubClientConfig::class]
 )
 interface GithubRepoInfoClient {
 
     @GetMapping("/user/orgs")
-    fun getOrgInfo(
-        @RequestHeader("Authorization") gitToken: String
-    ): List<GithubOrgInfoResponse>
+    fun getOrgInfo(): List<GithubOrgInfoResponse>
 
     @GetMapping("/user/repos")
     fun getRepoInfo(
-        @RequestHeader("Authorization") gitToken: String,
         @RequestParam(required = false) affiliation: String? = null
     ): List<GithubRepoInfoResponse>
 
     @GetMapping("/orgs/{orgName}/repos")
     fun getOrgRepoInfo(
-        @RequestHeader("Authorization") gitToken: String,
         @PathVariable(required = true) orgName: String
     ): List<GithubRepoInfoResponse>
 
     @GetMapping("/repos/{owner}/{repoName}/branches")
     fun getBranchInfo(
-        @RequestHeader("Authorization") gitToken: String,
         @PathVariable(required = true) owner: String,
         @PathVariable(required = true) repoName: String
     ): List<GithubBranchInfoResponse>
 
     @GetMapping("/repos/{owner}/{repoName}/contents/{path}")
     fun getItemInfo(
-        @RequestHeader("Authorization") gitToken: String,
         @PathVariable(required = true) owner: String,
         @PathVariable(required = true) repoName: String,
         @PathVariable(required = false) path: String,
@@ -48,7 +43,6 @@ interface GithubRepoInfoClient {
 
     @GetMapping("/repos/{owner}/{repoName}/git/trees/{sha}")
     fun getTreeInfo(
-        @RequestHeader("Authorization") gitToken: String,
         @PathVariable(required = true) owner: String,
         @PathVariable(required = true) repoName: String,
         @PathVariable(required = true) sha: String,
