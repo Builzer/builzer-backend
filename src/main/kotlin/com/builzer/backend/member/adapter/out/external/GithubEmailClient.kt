2e15c41fc9f216f1418a84a394b4f086ff5e3f80
wakkpu <ho0524870@gmail.com>
2024-07-21 23:46:55 +0900

    [FIX] email이 public이 아닌 경우 scope를 통해 user를 받아도 가져올 수 없음

---
+++ src/main/kotlin/com/builzer/backend/member/adapter/out/external/GithubEmailClient.kt
@@ -0,0 +1,18 @@
+package com.builzer.backend.member.adapter.out.external
+
+import com.builzer.backend.global.config.feign.GithubOAuthClientConfig
+import com.builzer.backend.member.adapter.out.external.response.UserEmail
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+
+@FeignClient(
+    name = "GithubMemberClient",
+    url = "\${feign.endpoint.github.api}",
+    configuration = [GithubOAuthClientConfig::class]
+)
+interface GithubEmailClient {
+    @GetMapping("/user/emails")
+    fun getUserEmails(@RequestHeader headers: HttpHeaders): List<UserEmail>
+}
